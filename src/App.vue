4ef4cef53c8d3e71b8a7ea2fa132c58696af7bfe
Alexandr <sasha-is-permi@mail.ru>
2021-11-15 20:31:09 +0500

    final

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,8 @@ export default {
     return{
       events:[
        {id:1,name:"Презентация",date: new Date(2021, 10, 18, 16,30), type:"red"},
-       {id:2,name:"Лабораторная",date: new Date(2021, 10, 18, 18,30), type:"green"},
-       {id:3,name:"семинар",date: new Date(2021, 10, 18, 18,30), type:"orange"},
        {id:4,name:"Лабораторная",date: new Date(2021, 10, 24, 14), type:"green"},
        {id:5,name:"семинар",date: new Date(2021, 11, 24, 16), type:"orange"},
-       {id:6,name:"Презентация",date: new Date(2021, 10, 18, 16,30), type:"red"},
        {id:7,name:"Лабораторная",date: new Date(2021, 10, 18, 18,30), type:"green"},
        {id:8,name:"семинар",date: new Date(2021, 11, 10, 18,30), type:"orange"},
        {id:9,name:"Лабораторная",date: new Date(2021, 10, 22, 14), type:"green"},
